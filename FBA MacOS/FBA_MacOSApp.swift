1dacde6e716359703c356ee370b8dcea0822f3bb
rparham1997 <rparham1997@gmail.com>
2024-06-04 10:34:37 -0400

    Initial Commit

---
+++ FBA MacOS/FBA_MacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FBA_MacOSApp.swift
+//  FBA MacOS
+//
+//  Created by Ramar Parham on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct FBA_MacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
